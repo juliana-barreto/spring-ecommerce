92317038c82ce914c8b2fb4cbc9db2864b8dd3b3
Juliana <89608026+juliana-barreto@users.noreply.github.com>
2026-02-08 09:16:18 -0300

    Add test data for orders and categories in TestConfig

--- src/main/java/com/juliana_barreto/ecommerce/TestConfig.java
+++ src/main/java/com/juliana_barreto/ecommerce/TestConfig.java
@@ -1,7 +1,13 @@
 package com.juliana_barreto.ecommerce;
 
+import com.juliana_barreto.ecommerce.modules.order.Order;
+import com.juliana_barreto.ecommerce.modules.order.OrderRepository;
+import com.juliana_barreto.ecommerce.modules.order.OrderStatus;
+import com.juliana_barreto.ecommerce.modules.product.Category;
+import com.juliana_barreto.ecommerce.modules.product.CategoryRepository;
 import com.juliana_barreto.ecommerce.modules.user.User;
 import com.juliana_barreto.ecommerce.modules.user.UserRepository;
+import java.time.Instant;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -15,6 +21,11 @@ public class TestConfig implements CommandLineRunner {
   @Autowired
   private UserRepository userRepository;
 
+  @Autowired
+  private OrderRepository orderRepository;
+
+  @Autowired
+  private CategoryRepository categoryRepository;
 
   @Override
   public void run(String... args) throws Exception {
@@ -35,5 +46,31 @@ public class TestConfig implements CommandLineRunner {
         .build();
 
     userRepository.saveAll(List.of(u1, u2));
+
+    Order o1 = Order.builder()
+        .moment(Instant.parse("2019-06-20T19:53:07Z"))
+        .status(OrderStatus.PAID)
+        .client(u1)
+        .build();
+
+    Order o2 = Order.builder()
+        .moment(Instant.parse("2019-07-21T03:42:10Z"))
+        .status(OrderStatus.AWAITING_PAYMENT)
+        .client(u2)
+        .build();
+
+    Order o3 = Order.builder()
+        .moment(Instant.parse("2019-07-22T15:21:22Z"))
+        .status(OrderStatus.AWAITING_PAYMENT)
+        .client(u1)
+        .build();
+
+    orderRepository.saveAll(List.of(o1, o2, o3));
+
+    Category cat1 = new Category(null, "Electronics");
+    Category cat2 = new Category(null, "Books");
+    Category cat3 = new Category(null, "Computers");
+
+    categoryRepository.saveAll(List.of(cat1, cat2, cat3));
   }
 }
