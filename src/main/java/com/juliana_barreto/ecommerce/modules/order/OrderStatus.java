f80d2fa5b3dc680bbff7f4f25473f61944d46e19
Juliana <89608026+juliana-barreto@users.noreply.github.com>
2026-02-12 09:58:50 -0300

    Refactor OrderStatus enum to simplify code structure

--- src/main/java/com/juliana_barreto/ecommerce/modules/order/OrderStatus.java
+++ src/main/java/com/juliana_barreto/ecommerce/modules/order/OrderStatus.java
@@ -7,20 +7,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum OrderStatus {
-  AWAITING_PAYMENT(1),
-  PAID(2),
-  SHIPPED(3),
-  DELIVERED(4),
-  CANCELED(5);
-
-  private final int code;
-
-  public static OrderStatus fromCode(int code) {
-    for (OrderStatus value : OrderStatus.values()) {
-      if (value.getCode() == code) {
-        return value;
-      }
-    }
-    throw new IllegalArgumentException("Invalid OrderStatus code: " + code);
-  }
+  AWAITING_PAYMENT,
+  PAID,
+  SHIPPED,
+  DELIVERED,
+  CANCELED;
 }
