ab6b1538b5463f51dd47835fab41dcbb5381b2ba
Juliana <89608026+juliana-barreto@users.noreply.github.com>
2026-02-12 11:12:20 -0300

    Add security filter chain configuration to SecurityConfig

--- src/main/java/com/juliana_barreto/ecommerce/shared/infra/SecurityConfig.java
+++ src/main/java/com/juliana_barreto/ecommerce/shared/infra/SecurityConfig.java
@@ -2,11 +2,32 @@ package com.juliana_barreto.ecommerce.shared.infra;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+
+    http
+        .csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+            .requestMatchers("/h2-console/**").permitAll()
+            .anyRequest().permitAll()
+        )
+        .headers(headers -> headers.frameOptions(FrameOptionsConfig::disable));
+
+    return http.build();
+  }
 
   @Bean
   public PasswordEncoder passwordEncoder() {
