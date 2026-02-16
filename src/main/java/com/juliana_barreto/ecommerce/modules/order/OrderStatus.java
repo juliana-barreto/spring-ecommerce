45617a5d788d5f108b26d5f45b5d9a9c0313be89
Juliana <89608026+juliana-barreto@users.noreply.github.com>
2026-02-16 11:59:36 -0300

    Refactor OrderStatus by removing Lombok annotations
    Removed Lombok annotations from OrderStatus enum.

--- src/main/java/com/juliana_barreto/ecommerce/modules/order/OrderStatus.java
+++ src/main/java/com/juliana_barreto/ecommerce/modules/order/OrderStatus.java
@@ -1,11 +1,5 @@
 package com.juliana_barreto.ecommerce.modules.order;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum OrderStatus {
   AWAITING_PAYMENT,
   PAID,
