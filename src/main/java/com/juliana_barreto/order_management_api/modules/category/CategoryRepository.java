1abd549a96946ba7230a8cd8778265d415dc258d
Juliana <89608026+juliana-barreto@users.noreply.github.com>
2026-02-20 01:12:39 -0300

    Add methods to find Category by name and User by email, CPF, and phone

--- src/main/java/com/juliana_barreto/order_management_api/modules/category/CategoryRepository.java
+++ src/main/java/com/juliana_barreto/order_management_api/modules/category/CategoryRepository.java
@@ -1,9 +1,11 @@
 package com.juliana_barreto.order_management_api.modules.category;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
+  Optional<Category> findByName(String name);
 }
--- src/main/java/com/juliana_barreto/order_management_api/modules/user/UserRepository.java
+++ src/main/java/com/juliana_barreto/order_management_api/modules/user/UserRepository.java
@@ -1,9 +1,15 @@
 package com.juliana_barreto.order_management_api.modules.user;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+  Optional<User> findByEmail(String email);
+
+  Optional<User> findByCpf(String cpf);
+
+  Optional<User> findByPhone(String phone);
 }
